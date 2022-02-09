fb4732aef40544ab15bb1734e21bc285b673631a
JehadJitan <63206085+JehadJitan@users.noreply.github.com>
2022-02-09 18:20:09 +0200

    myProfile

--- src/Pages/MyProfile/profileSetting.js
+++ src/Pages/MyProfile/profileSetting.js
@@ -1,6 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 import { ButtomTitleLine, TitleDiv } from '../../Components/Divs/StyledDivs'
+import mainLogo from './icon.png'
 
+const ParentDiv = styled.div`
+margin:100px;
+direction: rtl;
+display: flex;
+justify-content: center;
+
+`;
+const RightDiv = styled.div`
+padding:30px;
+width:450px;
+height:300px;
+background:#543920;
+display:flex;
+flex-direction: column;
+border-radius: 25px;
+box-shadow: 2px 10px 18px #888888;
+background-color: #cccccc;
+
+`;
+
+const LeftDiv = styled.div`
+margin-right:50px;
+padding:30px;
+width:450px;
+height:300px;
+background:#543920;
+display:flex;
+flex-direction: column;
+border-radius: 25px;
+box-shadow: 2px 10px 18px #888888;
+background-color: #cccccc;
+justify-content:center;
+align-items: center;
+
+`;
+
+const Label = styled.label`
+margin-left:10px;
+`;
+
+const InnerDiv = styled.div`
+margin-bottom:25px;
+`;
 
 function profileSetting() {
 
@@ -10,6 +55,34 @@ function profileSetting() {
                 <ButtomTitleLine>
                     <h1>تفاصيل حسابي</h1>
                 </ButtomTitleLine>
+                <ParentDiv>
+                    <RightDiv>
+                        <InnerDiv>
+                            <Label>اسم المستخدم</Label>
+                            <input type='text'></input>
+                        </InnerDiv>
+                        <InnerDiv>
+                            <Label>البريد الإلكتروني</Label>
+                            <input type='text'></input>
+                        </InnerDiv>
+                        <InnerDiv>
+                            <Label>تاريخ الميلاد</Label>
+                            <input type='text' style={{ 'margin-right': '25px' }}></input>
+                        </InnerDiv>
+                        <InnerDiv>
+                            <Label>رقم الهاتف</Label>
+                            <input type='text' style={{ 'margin-right': '28px' }}></input>
+                        </InnerDiv>
+                        <InnerDiv>
+                            <span>لتغيير كلمة المرور إضغط <a style={{ 'color': '#d31818' }} href='#'>هنا</a></span>
+                        </InnerDiv>
+                    </RightDiv>
+                    <LeftDiv>
+                        <img src={mainLogo} alt="khadamatiLogo" width={200} style={{ 'margin-bottom': '25px' }} />
+                        <span>لتغيير الصورة الشخصية إضغط <a style={{ 'color': '#d31818' }} href='#'>هنا</a></span>
+
+                    </LeftDiv>
+                </ParentDiv>
             </TitleDiv>
         </>
     );
