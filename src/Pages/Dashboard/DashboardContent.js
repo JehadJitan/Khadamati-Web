91f608a9bf0a7bba0dcfdb192382fdb251fda595
JehadJitan <63206085+JehadJitan@users.noreply.github.com>
2022-02-12 13:06:01 +0200

    change in dashboard

--- src/Pages/Dashboard/DashboardContent.js
+++ src/Pages/Dashboard/DashboardContent.js
@@ -24,6 +24,16 @@ const Parent = styled.div`
     margin: 50px;
 `;
 
+const Parent3 = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: row;
+    align-items: center;
+    margin: 50px;
+    flex-wrap: wrap;
+
+`;
+
 const Right = styled.div`
     width: 210px;
     height: 310px;
@@ -53,9 +63,22 @@ const Center = styled.div`
     margin-top: 80px;
 `;
 
+const Card = styled.div`
+    width: 330px;
+    height: 100px;
+    padding: 20px;
+    border-radius: 25px;
+    box-shadow: 5px 10px 18px #888888;
+    background-color: #fdc500;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right:50px;
+`;
+
 const Left = styled.div`
-    width: 210px;
-    height: 310px;
+    width: 200px;
+    height: 300px;
     padding: 20px;
     border-radius: 25px;
     text-align: center;
@@ -78,20 +101,43 @@ const Title = styled.h1`
     margin-bottom: 5px;
 `;
 
+const Title4 = styled.h1`
+    color: black;
+    font-size: 18px;
+    margin-right: 10px;
+    width:200px;
+    text-align: right;
+    `;
+
+const Title5 = styled.h1`
+    color: black;
+    font-size: 23px;
+    text-align: right;
+    font-weight: bold;
+
+`;
+
 const Parent2 = styled.div`
     display: flex;
     justify-content: center;
-    width: 700px;
-    height: 320px;
+    width: 750px;
+    height: 300px;
     padding: 20px;
     border-radius: 25px;
     box-shadow: 5px 10px 18px #888888;
     background-color: #ffffff;
     align-items: center;
     margin: 0 auto;
-    margin-top:50px;
     flex-direction: row;
 
+`;
+
+const STY2 = styled.div`
+margin-top:30px;
+width:200px;
+height:100px;
+display: flex;
+flex-direction: column;
 `;
 
 function DashboardContent() {
@@ -107,67 +153,98 @@ function DashboardContent() {
                     <h1>نظرة عامة على لوحة القيادة</h1>
                 </ButtomTitleLine>
             </TitleDiv>
-            <Parent2>
-                <table border="1" width="700px">
-                    <tr>
-                        <th width="150px">عدد الطلبات</th>
-                        <th width="150px">عدد الخدمات</th>
-                        <th width="150px">عدد الموظفين</th>
-                        <th width="250px">إسم الوزارة</th>
-                    </tr>
-
-                    <tr>
-                        <td>{MOTRequestLength}</td>
-                        <td>{MOTServiceRowLength}</td>
-                        <td>{MOTRowLength}</td>
-                        <td style={{ 'color': '#d31818' }}>وزارة المواصلات</td>
-                    </tr>
-                    <tr>
-                        <td>{MOIRequestLength}</td>
-                        <td>{MOIServiceRowLength}</td>
-                        <td>{MOIERowLength}</td>
-                        <td style={{ 'color': '#d31818' }}>وزارة الداخلية</td>
-                    </tr>
-                    <tr>
-                        <td>{MOFRequestLength}</td>
-                        <td>{MOFEServiceRowLength}</td>
-                        <td>{MOFERowLength}</td>
-                        <td style={{ 'color': '#d31818' }}>وزارة المالية</td>
-                    </tr>
-                    <tr>
-                        <td>{MOTRequestLength}</td>
-                        <td>{MOTServiceRowLength}</td>
-                        <td>{MOTRowLength}</td>
-                        <td style={{ 'color': '#d31818' }}>وزارة الصحة</td>
-                    </tr>
-                    <tr>
-                        <td>{MOPRequestLength}</td>
-                        <td>{MOPServiceRowLength}</td>
-                        <td>{MOPRowLength}</td>
-                        <td style={{ 'color': '#d31818' }}>سلطة الأراضي</td>
-                    </tr>
-                </table>
-            </Parent2>
+            <Parent3>
+                <Card style={{ 'background': '#38b000' }}>
+                    <STY2 style={{ 'margin-top': '35px', 'color': 'black' }}>
+                        <BsIcons.BsPeopleFill size={60} />
+                    </STY2>
+                    <STY2>
+                        <Title5>100</Title5>
+                        <Title4>عدد مستخدمين التطبيق </Title4>
+                    </STY2>
+                </Card>
+                <Card>
+                    <STY2 style={{ 'margin-top': '35px', 'color': 'black' }}>
+                        <BsIcons.BsPeopleFill size={60} />
+                    </STY2>
+                    <STY2>
+                        <Title5>{totalEmployees}</Title5>
+                        <Title4>عدد الموظفين الحكومية</Title4>
+                    </STY2>
+                </Card>
+                <Card style={{ 'background': '#38b6ff' }}>
+                    <STY2 style={{ 'margin-top': '35px', 'color': 'black' }}>
+                        <BsIcons.BsPeopleFill size={60} />
+                    </STY2>
+                    <STY2>
+                        <Title5>{numOfCitizens}</Title5>
+                        <Title4>العدد الإجمالي للمواطنين</Title4>
+                    </STY2>
+                </Card>
+            </Parent3>
             <Parent>
-                <Left>
-                    <BsIcons.BsPeopleFill /><Title>عدد موظفين القطاعات الحكومية</Title>
-                    <h2>{totalEmployees}</h2>
-                    <Circle>
-                        <CircularProgressbar styles={buildStyles({ trailColor: '#f5f5f5', pathColor: '#d31818', textColor: '#d31818' })} value={Empercentage} text={`${Empercentage}%`} />;
-                    </Circle>
-                </Left>
-                <Center>
+                {/* <Parent2>
+                    <Left>
+                        <BsIcons.BsPeopleFill /><Title>عدد موظفين القطاعات الحكومية</Title>
+                        <h2>{totalEmployees}</h2>
+                        <Circle>
+                            <CircularProgressbar styles={buildStyles({ trailColor: '#f5f5f5', pathColor: '#d31818', textColor: '#d31818' })} value={Empercentage} text={`${Empercentage}%`} />;
+                        </Circle>
+                    </Left>
+                    <Center>
 
-                    <BsIcons.BsPeopleFill /><Title>العدد الإجمالي للمواطنين</Title>
-                    <h2>{numOfCitizens}</h2>
-                </Center>
-                <Right>
-                    <BsIcons.BsPeopleFill /><Title>عدد مستخدمين تطبيق خدماتي</Title>
-                    <h2>{NumOfUsers}</h2>
-                    <Circle>
-                        <CircularProgressbar styles={buildStyles({ trailColor: '#f5f5f5', pathColor: '#d31818', textColor: '#d31818' })} value={Usprecentage} text={`${Usprecentage}%`} />;
-                    </Circle>
-                </Right>
+                        <BsIcons.BsPeopleFill /><Title>العدد الإجمالي للمواطنين</Title>
+                        <h2>{numOfCitizens}</h2>
+                    </Center>
+                    <Right>
+                        <BsIcons.BsPeopleFill /><Title>عدد مستخدمين تطبيق خدماتي</Title>
+                        <h2>{NumOfUsers}</h2>
+                        <Circle>
+                            <CircularProgressbar styles={buildStyles({ trailColor: '#f5f5f5', pathColor: '#d31818', textColor: '#d31818' })} value={Usprecentage} text={`${Usprecentage}%`} />;
+                        </Circle>
+                    </Right>
+                </Parent2> */}
+                <Parent2>
+                    <table border="1" width="700px">
+                        <tr>
+                            <th width="150px">عدد الطلبات</th>
+                            <th width="150px">عدد الخدمات</th>
+                            <th width="150px">عدد الموظفين</th>
+                            <th width="250px">إسم الوزارة</th>
+                        </tr>
+
+                        <tr>
+                            <td>{MOTRequestLength}</td>
+                            <td>{MOTServiceRowLength}</td>
+                            <td>{MOTRowLength}</td>
+                            <td style={{ 'color': '#d31818' }}>وزارة المواصلات</td>
+                        </tr>
+                        <tr>
+                            <td>{MOIRequestLength}</td>
+                            <td>{MOIServiceRowLength}</td>
+                            <td>{MOIERowLength}</td>
+                            <td style={{ 'color': '#d31818' }}>وزارة الداخلية</td>
+                        </tr>
+                        <tr>
+                            <td>{MOFRequestLength}</td>
+                            <td>{MOFEServiceRowLength}</td>
+                            <td>{MOFERowLength}</td>
+                            <td style={{ 'color': '#d31818' }}>وزارة المالية</td>
+                        </tr>
+                        <tr>
+                            <td>{MOTRequestLength}</td>
+                            <td>{MOTServiceRowLength}</td>
+                            <td>{MOTRowLength}</td>
+                            <td style={{ 'color': '#d31818' }}>وزارة الصحة</td>
+                        </tr>
+                        <tr>
+                            <td>{MOPRequestLength}</td>
+                            <td>{MOPServiceRowLength}</td>
+                            <td>{MOPRowLength}</td>
+                            <td style={{ 'color': '#d31818' }}>سلطة الأراضي</td>
+                        </tr>
+                    </table>
+                </Parent2>
             </Parent>
         </>
     );
