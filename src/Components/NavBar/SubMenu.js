7f88dd664ba093fb25d690a425d9f8b9ac8856a3
TarekKhoure <sweetman.1999@hotmail.com>
2022-04-22 13:18:00 +0300

    sidebar fixed - get Image api added

--- src/Components/NavBar/SubMenu.js
+++ src/Components/NavBar/SubMenu.js
@@ -1,99 +1,114 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom'
-import styled from 'styled-components';
-import 'simplebar'; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
-import 'simplebar/dist/simplebar.css';
+import React, { useEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import "simplebar"; // or "import SimpleBar from 'simplebar';" if you want to use it manually.
+import "simplebar/dist/simplebar.css";
 
 const SideBarLink = styled(Link)`
-    display:flex;
-    color: #e1e9fc;
-    justify-content: space-between;
-    padding:20px;
-    align-items:center;
-    height:60px;
-    list-style:none;
-    text-decoration:none;
-    font-size:15px;
+  display: flex;
+  color: #e1e9fc;
+  justify-content: space-between;
+  padding: 20px;
+  align-items: center;
+  height: 60px;
+  list-style: none;
+  text-decoration: none;
+  font-size: 15px;
 
-    &:hover{
-      background: #252831;
-      border-right:4px solid #d31818;
-      cursor:pointer;  
-    }
+  &:hover {
+    background: #252831;
+    border-right: 4px solid #d31818;
+    cursor: pointer;
+  }
 `;
 
 const SideBarLabel = styled.span`
-    margin-right:16px;
+  margin-right: 16px;
 `;
 
 const DropdownLink = styled(Link)`
-    background: #414757;
-    height:60px;
-    padding-left: 8rem;
-    display:flex;
-    align-items:center;
-    text-decoration:none;
-    color:#f5f5f5;
-    font-size:15px;
+  background: #414757;
+  height: 60px;
+  padding-left: 8rem;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: #f5f5f5;
+  font-size: 15px;
 
-    &:hover{
-        background:#252831;
-        border-right: 4px solid #d31818;
-        cursor:pointer;
-    }
+  &:hover {
+    background: #252831;
+    border-right: 4px solid #d31818;
+    cursor: pointer;
+  }
 `;
 
 function useOutsideAlerter(ref) {
-    useEffect(() => {
-        /**
-         * Alert if clicked on outside of element
-         */
-        function handleClickOutside(event) {
-            if (ref.current && !ref.current.contains(event.target)) {
-                alert("You clicked outside of me!");
-            }
+  useEffect(() => {
+    /**
+     * Alert if clicked on outside of element
+     */
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        alert("You clicked outside of me!");
+      }
+    }
+
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+}
+
+const SubMenu = ({ item, handleSideBarChange, sidebar }) => {
+  const [subnav, setSubnav] = useState(false);
+
+  const showSubnav = () => setSubnav(!subnav);
+
+  const elementRef = useRef(null);
+  useOutsideAlerter(elementRef);
+
+  return (
+    <div>
+      <SideBarLink
+        to={item.path}
+        onClick={
+          (item.subNav && showSubnav) ||
+          (item.title === "لائحة المستخدمين"
+            ? () => handleSideBarChange(!sidebar)
+            : null)
         }
-
-        // Bind the event listener
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            // Unbind the event listener on clean up
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [ref]);
-}
-
-const SubMenu = ({ item }) => {
-
-    const [subnav, setSubnav] = useState(false);
-
-    const showSubnav = () => setSubnav(!subnav);
-
-
-    const elementRef = useRef(null);
-    useOutsideAlerter(elementRef)
-
-    return (
+      >
         <div>
-            <SideBarLink to={item.path} onClick={item.subNav && showSubnav}>
-                <div >
-                    {/* null means if there is no extension then dont put arrow */}
-                    {item.subNav && subnav ? item.iconOpened : item.subNav ? item.iconClosed : null}
-                </div>
-                <div>
-                    <SideBarLabel>{item.title}</SideBarLabel>
-                    {item.icon}
-                </div>
-            </SideBarLink>
-            {subnav && item.subNav.map((item, index) => {
-                return (
-                    <DropdownLink to={item.path} key={index} >
-                        <SideBarLabel>{item.title}</SideBarLabel>
-                        {item.icon}
-                    </DropdownLink>
-                )
-            })}
-        </div >
-    )
-}
+          {/* null means if there is no extension then dont put arrow */}
+          {item.subNav && subnav
+            ? item.iconOpened
+            : item.subNav
+            ? item.iconClosed
+            : null}
+        </div>
+        <div>
+          <SideBarLabel>{item.title}</SideBarLabel>
+          {item.icon}
+        </div>
+      </SideBarLink>
+      {subnav &&
+        item.subNav.map((item, index) => {
+          return (
+            <DropdownLink
+              to={item.path}
+              key={index}
+              onClick={() => handleSideBarChange(!sidebar)}
+            >
+              <SideBarLabel>{item.title}</SideBarLabel>
+              {item.icon}
+            </DropdownLink>
+          );
+        })}
+    </div>
+  );
+};
 export default SubMenu;
--- src/shared/api.js
+++ src/shared/api.js
@@ -2,87 +2,98 @@ import axios from "axios";
 import { API_BASE_URL } from "./config";
 
 export const addEmployee = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/employee`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/employee`, { data });
+  return res;
 };
 
 export const getEmployees = async (role) => {
-    const res = await axios.get(`${API_BASE_URL}/employees/${role}`);
-    return res;
+  const res = await axios.get(`${API_BASE_URL}/employees/${role}`);
+  return res;
 };
 
 export const editEmployee = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/employee/edit`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/employee/edit`, { data });
+  return res;
 };
 
 export const getCitizens = async (data) => {
-    const res = await axios.get(`${API_BASE_URL}/citizens`);
-    return res;
+  const res = await axios.get(`${API_BASE_URL}/citizens`);
+  return res;
 };
 
 export const getRowsLength = async (role) => {
-    const res = await axios.get(`${API_BASE_URL}/employees/rowLength`);
-    return res;
+  const res = await axios.get(`${API_BASE_URL}/employees/rowLength`);
+  return res;
 };
 
 export const editService = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/service/edit`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/service/edit`, { data });
+  return res;
 };
 
 export const addService = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/service`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/service`, { data });
+  return res;
 };
 
 export const getService = async (type) => {
-    const res = await axios.get(`${API_BASE_URL}/service/${type}`);
-    return res;
+  const res = await axios.get(`${API_BASE_URL}/service/${type}`);
+  return res;
 };
 
 export const getAdmin = async (email) => {
-    console.log(email);
-    const res = await axios.get(`${API_BASE_URL}/admin/${email}`);
-    return res;
+  console.log(email);
+  const res = await axios.get(`${API_BASE_URL}/admin/${email}`);
+  return res;
 };
 
-
 export const addRequest = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/request`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/request`, { data });
+  return res;
 };
 
 export const getRequest2 = async (role) => {
-    const res = await axios.get(`${API_BASE_URL}/request/role/${role}`);
-    return res.data.data;
+  const res = await axios.get(`${API_BASE_URL}/request/role/${role}`);
+  return res.data.data;
 };
 
 export const getDashboardContent = async (data) => {
-    const res = await axios.get(`${API_BASE_URL}/dashboard`);
-    return res;
+  const res = await axios.get(`${API_BASE_URL}/dashboard`);
+  return res;
 };
 
 export const editStatus = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/citizen/status`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/citizen/status`, { data });
+  return res;
 };
 
 export const acceptRequest = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/citizen/accept/request`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/citizen/accept/request`, {
+    data,
+  });
+  return res;
 };
 export const addVaccinatedCitizen = async (data) => {
-    const res = await axios.post(`${API_BASE_URL}/covid`, { data });
-    return res;
+  const res = await axios.post(`${API_BASE_URL}/covid`, { data });
+  return res;
 };
 
 export const editVaccinatedCitizen = async (data) => {
-    const res = await axios.get(`${API_BASE_URL}/covid/edit`, { data });
-    return res;
+  const res = await axios.get(`${API_BASE_URL}/covid/edit`, { data });
+  return res;
 };
 
 export const getVaccinatedCitizen = async (type) => {
-    const res = await axios.get(`${API_BASE_URL}/covid/${type}`);
-    return res;
+  const res = await axios.get(`${API_BASE_URL}/covid/${type}`);
+  return res;
+};
+
+export const getCurrentImage = async (data) => {
+  const res = await axios.get(`${API_BASE_URL}/images/${data}`);
+  return res;
+};
+
+export const getNewImage = async (data) => {
+  const res = await axios.get(`${API_BASE_URL}/images/New/${data}`);
+  return res;
 };
