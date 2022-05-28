8ff5a49a4d188002c616d86faff287368b93f1d4
JehadJitan <63206085+JehadJitan@users.noreply.github.com>
2022-05-28 12:09:37 +0300

    archived transferland page modification

--- src/Pages/MOP/transferLandHistory.js
+++ src/Pages/MOP/transferLandHistory.js
@@ -1,17 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { DataGrid } from "@mui/x-data-grid";
-import {
-  ButtomTitleLine,
-  StyledTable,
-  TitleDiv,
-  InsideDivTitle,
-} from "../../Components/Divs/StyledDivs";
+import { ButtomTitleLine, TitleDiv } from "../../Components/Divs/StyledDivs";
 import Button from "@mui/material/Button";
 import styled from "styled-components";
 import { getTransferredLands } from "../../shared/api";
 import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
-import AccountCircle from "@mui/icons-material/AccountCircle";
 
 export const StyledParent = styled.div`
   display: flex;
@@ -25,17 +17,17 @@ export const StyledParent = styled.div`
   box-shadow: 5px 10px 18px #888888;
   background-color: #ffffff;
   direction: rtl;
-  overflow:scroll;
+  overflow: scroll;
   overflow-x: hidden;
 `;
 
 export const StyledToolBar = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   flex-wrap: wrap;
-//   align-items: center;
-//   margin: auto;
-  display: inline-block; 
+  //   display: flex;
+  //   flex-direction: row;
+  //   flex-wrap: wrap;
+  //   align-items: center;
+  //   margin: auto;
+  display: inline-block;
   width: 100%;
   height: 70px;
   margin-top: 30px;
@@ -72,12 +64,12 @@ export const StyledLabel = styled.label`
 `;
 
 export const StyledTextInToolbar = styled.label`
-//   font-size: 17px;
+  //   font-size: 17px;
   color: #d31818;
   float: left;
-  margin-top: 10px;
+  margin-top: 20px;
   margin-left: 20px;
-  `;
+`;
 
 export const StyledLabelText = styled.label`
   margin-bottom: 10px;
@@ -101,11 +93,26 @@ export default function InteriorRequestTable() {
   }, []);
 
   const [inputText, setInputText] = useState("");
+  const [inputTextOldOwner, setInputTextOldOwner] = useState("");
   let inputHandler = (e) => {
     var text = e.target.value;
     setInputText(text);
     // console.log(text);
   };
+
+  let inputHandlerOldOwner = (e) => {
+    var text = e.target.value;
+    setInputTextOldOwner(text);
+    // console.log(text);
+  };
+
+  const filteredDataOldOwner = data.filter((value) => {
+    if (inputTextOldOwner == value.ownerId) {
+      return value.toString().includes(value.ownerId);
+    } else {
+      return value;
+    }
+  });
 
   const filteredData = data.filter((value) => {
     if (inputText == value.oldOwnerId) {
@@ -114,6 +121,10 @@ export default function InteriorRequestTable() {
       return value;
     }
   });
+  function ClearFields() {
+    document.getElementById("outlined-newOwner-input").value = "";
+    // document.getElementById("outlined-oldOwner-input").value = "";
+}
 
   return (
     <>
@@ -123,9 +134,13 @@ export default function InteriorRequestTable() {
         </ButtomTitleLine>
       </TitleDiv>
       <StyledToolBar>
-        <TextField 
-        style={{'float': 'right',   'margin-top': '5px', 'margin-right': '20px'}}
-          id="outlined-password-input"
+        <TextField
+          style={{
+            float: "right",
+            "margin-top": "15px",
+            "margin-right": "20px",
+          }}
+          id="outlined-newOwner-input"
           label="البحث عن طريق هوية المالك الجديد"
           type="text"
           textAlign="center"
@@ -134,57 +149,83 @@ export default function InteriorRequestTable() {
           size="small"
           color="error"
         />
+        <TextField
+          style={{
+            float: "right",
+            "margin-top": "15px",
+            "margin-right": "20px",
+          }}
+          id="outlined-oldOwner-input"
+          label="البحث عن طريق هوية المالك القديم"
+          type="text"
+          textAlign="center"
+          autoComplete="current-password"
+          onChange={inputHandlerOldOwner}
+          size="small"
+          color="error"
+        />
+        <Button
+          onClick={ClearFields}
+          variant="contained"
+          style={{
+            "margin-top": "15px",
+            "margin-right": "20px",
+            "font-family": "Almarai",
+            "backgroundColor": "#d31818",
+          }}
+        >
+          حذف المدخلات
+        </Button>
         <StyledTextInToolbar>
-            العدد الإجمالي للطلبات هو: {filteredData.length}
+          العدد الإجمالي للطلبات هو: {filteredData.length}
         </StyledTextInToolbar>
       </StyledToolBar>
       <StyledParent>
-
-      {filteredData.map((request) => {
-        return (
-          <StyledView>
-            <StyledColumn>
-              <StyledLabel>إسم المالك الأول:</StyledLabel>
-              <StyledLabel>رقم هوية المالك الأول:</StyledLabel>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabelText>{request.oldOwner}</StyledLabelText>
-              <StyledLabelText>{request.oldOwnerId}</StyledLabelText>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabel>إسم المالك الجيديد:</StyledLabel>
-              <StyledLabel>رقم هوية المالك الجيديد:</StyledLabel>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabelText>{request.owner}</StyledLabelText>
-              <StyledLabelText>{request.ownerId}</StyledLabelText>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabel>رقم القطعة:</StyledLabel>
-              <StyledLabel>رقم الحوض:</StyledLabel>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabelText>{request.pieceId}</StyledLabelText>
-              <StyledLabelText>{request.hodId}</StyledLabelText>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabel>سعر البيع:</StyledLabel>
-              <StyledLabel>المساحة الإجمالية:</StyledLabel>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabelText>{request.totalPrice}</StyledLabelText>
-              <StyledLabelText>{request.totalArea}</StyledLabelText>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabel>تاريخ البيع:</StyledLabel>
-            </StyledColumn>
-            <StyledColumn>
-              <StyledLabelText>{request.submitDate}</StyledLabelText>
-            </StyledColumn>
-          </StyledView>
-        );
-      })}
-            </StyledParent>
+        {(inputTextOldOwner === "")&&(filteredData.map((request) => {
+          return (
+            <StyledView>
+              <StyledColumn>
+                <StyledLabel>إسم المالك الأول:</StyledLabel>
+                <StyledLabel>رقم هوية المالك الأول:</StyledLabel>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabelText>{request.oldOwner}</StyledLabelText>
+                <StyledLabelText>{request.oldOwnerId}</StyledLabelText>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabel>إسم المالك الجيديد:</StyledLabel>
+                <StyledLabel>رقم هوية المالك الجيديد:</StyledLabel>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabelText>{request.owner}</StyledLabelText>
+                <StyledLabelText>{request.ownerId}</StyledLabelText>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabel>رقم القطعة:</StyledLabel>
+                <StyledLabel>رقم الحوض:</StyledLabel>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabelText>{request.pieceId}</StyledLabelText>
+                <StyledLabelText>{request.hodId}</StyledLabelText>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabel>سعر البيع:</StyledLabel>
+                <StyledLabel>المساحة الإجمالية:</StyledLabel>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabelText>{request.totalPrice}₪</StyledLabelText>
+                <StyledLabelText>{request.totalArea}m²</StyledLabelText>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabel>تاريخ البيع:</StyledLabel>
+              </StyledColumn>
+              <StyledColumn>
+                <StyledLabelText>{request.submitDate}</StyledLabelText>
+              </StyledColumn>
+            </StyledView>
+          );
+        }))}
+      </StyledParent>
     </>
   );
 }
